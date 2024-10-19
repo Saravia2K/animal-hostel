b3ebcb0f3b17220ea7c5e9190598f49051e4c657
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-10-18 22:04:45 -0600

    Invalidando queries de pets cuando un cliente haya sido actualizado

--- src/app/(pages)/dashboard/clientes/[id]/editar/page.tsx
+++ src/app/(pages)/dashboard/clientes/[id]/editar/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { useRouter } from "next-nprogress-bar";
 import { useParams } from "next/navigation";
+import { useQueryClient } from "@tanstack/react-query";
 
 import Title from "@/components/Title";
 import OwnerForm from "@/forms/OwnerForm";
@@ -15,9 +16,14 @@ export default function EditarClientePage() {
   const { owner, ownerLoading, reloadOwner } = useOwner(+id);
   const { setOpenState } = useLoadingOverlay();
   const router = useRouter();
+  const qc = useQueryClient();
 
   const handleSuccessForm = async () => {
     await reloadOwner();
+
+    for (const pet of owner!.pets)
+      qc.invalidateQueries({ queryKey: ["pets", pet.id_pet] });
+
     router.push(`/dashboard/clientes/${id}`);
   };
 
