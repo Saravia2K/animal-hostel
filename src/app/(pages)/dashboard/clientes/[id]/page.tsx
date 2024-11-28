2f200d22cf2660b080d83c34058591cd96dc9d93
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-11-28 09:35:05 -0600

    Usando TitleWithButton y PetsCardsList

--- src/app/(pages)/dashboard/clientes/[id]/page.tsx
+++ src/app/(pages)/dashboard/clientes/[id]/page.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import Image from "next/image";
 import { useParams } from "next/navigation";
 import { useRouter } from "next-nprogress-bar";
-import { Button, Grid2 as Grid, Typography } from "@mui/material";
+import { Grid2 as Grid } from "@mui/material";
 
-import Title from "@/components/Title";
 import CardInformation from "@/components/CardInformation";
+import TitleWithButton from "@/components/TitleWithButton";
+import PetsCardsList from "@/components/PetsCardsList";
 
 import useOwner from "@/hooks/useOwner";
 import useIsResponsive from "@/hooks/useIsResponsive";
-
-import mascotaIcon from "@/assets/images/mascota_icon.png";
 
 export default function ClienteDetailPage() {
   const { id } = useParams<{ id: string }>();
@@ -29,25 +27,15 @@ export default function ClienteDetailPage() {
         borderRadius={4}
         sx={{ backgroundColor: "#fff" }}
       >
-        <Grid
-          size={12}
-          display="flex"
-          justifyContent={isResponsive ? "initial" : "space-between"}
-          alignItems={isResponsive ? "initial" : "center"}
-          flexDirection={isResponsive ? "column" : "row"}
-          gap={isResponsive ? 2 : 0}
-        >
-          <Title text={`${owner?.names} ${owner?.last_names}`} mb={0} />
-          <Button
-            variant="contained"
-            sx={{ backgroundColor: "var(--orange)" }}
-            onClick={() =>
-              router.push(`/dashboard/clientes/${owner.id_owner}/editar`)
-            }
-          >
-            Editar cliente
-          </Button>
-        </Grid>
+        <TitleWithButton
+          grid
+          title={`${owner?.names} ${owner?.last_names}`}
+          buttonText="Editar cliente"
+          btnColor="orange"
+          onClick={() =>
+            router.push(`/dashboard/clientes/${owner.id_owner}/editar`)
+          }
+        />
 
         <Grid size={{ xs: 12, sm: 5 }}>
           <CardInformation header="Dirección:" text={owner.home} />
@@ -66,41 +54,7 @@ export default function ClienteDetailPage() {
         </Grid>
       </Grid>
 
-      <Grid container rowSpacing={3} columnSpacing={6} mt={5}>
-        <Grid
-          size={12}
-          fontWeight="bold"
-          pl={3}
-          pb={1}
-          borderBottom="1px solid grey"
-        >
-          Mascotas
-        </Grid>
-        {owner?.pets.map((p) => (
-          <Grid
-            key={p.id_pet}
-            size={{ xs: 12, sm: 6, md: 4, lg: 3 }}
-            borderRadius={3}
-            p={2}
-            display="grid"
-            gridTemplateColumns="25% 1fr"
-            gap={3}
-            alignItems="center"
-            sx={{ backgroundColor: "#7fc243", cursor: "pointer" }}
-            onClick={() => router.push(`/dashboard/mascotas/${p.id_pet}`)}
-          >
-            <Image src={mascotaIcon} alt="" />
-            <Typography
-              fontFamily="inherit"
-              fontSize={20}
-              fontWeight="bold"
-              color="#fff"
-            >
-              {p.name}
-            </Typography>
-          </Grid>
-        ))}
-      </Grid>
+      <PetsCardsList pets={owner.pets} />
     </>
   );
 }
