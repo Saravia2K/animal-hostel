55f0cf53b21c7acb7d0fcb2a9e5126489cacb3fb
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-06-01 09:10:06 -0600

    [Feat]: Simplifying function exitSameDateThatGotIn

--- src/forms/EntryForm/index.tsx
+++ src/forms/EntryForm/index.tsx
@@ -172,11 +172,16 @@ export default function EntryForm({ initialValues, onSuccessForm }: TProps) {
   const exitSameDateThatGotIn = () => {
     const inDateObj = new Date(inDate);
     const outDateObj = new Date(outDate);
-    return (
-      inDateObj.getDate() == outDateObj.getDate() &&
-      inDateObj.getMonth() == outDateObj.getMonth() &&
-      inDateObj.getFullYear() == outDateObj.getFullYear()
-    );
+
+    const formatDate = (date: Date) => {
+      const day = String(date.getDate()).padStart(2, "0");
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const year = date.getFullYear();
+
+      return `${day}/${month}/${year}`;
+    };
+
+    return formatDate(inDateObj) == formatDate(outDateObj);
   };
 
   const inDate = watch("entry_date");
