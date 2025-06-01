386a9dca3d30fe78adce2cbd2d9f3b16e4c3b98d
Diego Saravia <diego.saraviazaldana@gmail.com>
2025-06-01 09:10:38 -0600

    [Feat]: Setting locale "es" to LocalizationProvider for Calendar and Clock components

--- src/providers/LocalizationProvider.tsx
+++ src/providers/LocalizationProvider.tsx
@@ -3,10 +3,18 @@
 import { type PropsWithChildren } from "react";
 import { LocalizationProvider as MUILocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import "dayjs/locale/es";
+import { esES } from "@mui/x-date-pickers/locales";
 
 export default function LocalizationProvider({ children }: PropsWithChildren) {
   return (
-    <MUILocalizationProvider dateAdapter={AdapterDayjs}>
+    <MUILocalizationProvider
+      dateAdapter={AdapterDayjs}
+      adapterLocale="es"
+      localeText={
+        esES.components.MuiLocalizationProvider.defaultProps.localeText
+      }
+    >
       {children}
     </MUILocalizationProvider>
   );
